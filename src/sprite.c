61d841fefd1e9ee3b824d198707b6911bd7b763c
cppchriscpp <admin@cpprograms.net>
2017-12-02 01:59:56 -0500

    Add missing files

---
+++ src/sprite.c
@@ -0,0 +1,67 @@
+#include "src/globals.h"
+#include "lib/neslib.h"
+#include "src/sprite.h"
+
+#define PLAYER_START_ID 0
+
+const unsigned char sprite_data[] = {
+	// Player Start
+	SPRITE_TYPE_IGNORED, SPRITE_SIZE_NORMAL | SPRITE_PALETTE_1 | SPRITE_ANIM_NONE, 0xee, 0,
+    // Gem
+    // TODO: animate? (Or maybe we just chr swap)
+	SPRITE_TYPE_GEM, SPRITE_SIZE_NORMAL | SPRITE_PALETTE_2 | SPRITE_ANIM_NONE, 0x00, SPRITE_DIRECTION_DOWN,
+	// Some enemy, TBD
+	SPRITE_TYPE_ENEMY, SPRITE_SIZE_NORMAL | SPRITE_PALETTE_3 | SPRITE_ANIM_DEFAULT, 0x80, 0,
+};
+
+static char sprite_directions[12];
+static char sprite_direction_time[12];
+
+
+void banked_draw_sprites() {
+    // TODO: Figure out how to track enemy movement in this game.
+    /*
+	for (i = 0; i < 12; i++) {
+		sprite_directions[i] = SPRITE_DIRECTION_UNDEF;
+		sprite_direction_time[i] = SPRITE_MIN_DIRECTION_TIME-10;
+    }
+    */
+
+	for (i = 0; i < 12 && currentLevel[MAP_TILE_SIZE + (i<<1)] != 255; ++i) {
+		scratch = currentLevel[MAP_TILE_SIZE + (i<<1)];
+		scratch2 = currentLevel[MAP_TILE_SIZE + (i<<1)+1];
+
+		extendedSpriteData[(i<<2)  ] = sprite_data[(scratch2<<2)];
+		extendedSpriteData[(i<<2)+1] = sprite_data[(scratch2<<2)+1];
+		extendedSpriteData[(i<<2)+2] = sprite_data[(scratch2<<2)+2];
+		extendedSpriteData[(i<<2)+3] = sprite_data[(scratch2<<2)+3];
+
+        // Scratch 1 is the position. Need to do some hax to get it to 16x16 coords
+        // Why -1? Seems to align things to tiles, never figured that out in detail. Game jams make me do bad things >_>
+		scratch2 = ((scratch >> 4) << 4)-1; // Y
+		scratch = ((scratch % 16) << 4); // X
+		scratch3 = extendedSpriteData[(i<<2)+2];
+		// I'm so, so sorry.
+        scratch4 = sprite_data[((currentLevel[MAP_TILE_SIZE + (i<<1)+1])<<2)+1] & SPRITE_PALETTE_MASK;
+        
+        if (scratch2 == PLAYER_START_ID) { 
+            // Just hide me, then set the params for the player's starting position.
+            oam_spr(NESLIB_SPRITE_GONE, NESLIB_SPRITE_GONE, scratch3, scratch4, FIRST_ENEMY_SPRITE_ID + (i << 4));
+            oam_spr(NESLIB_SPRITE_GONE, NESLIB_SPRITE_GONE, scratch3+1, scratch4, (FIRST_ENEMY_SPRITE_ID + 4) + (i << 4));
+            oam_spr(NESLIB_SPRITE_GONE, NESLIB_SPRITE_GONE, scratch3+0x10, scratch4, (FIRST_ENEMY_SPRITE_ID + 8) + (i << 4));
+            currentSpriteId = oam_spr(NESLIB_SPRITE_GONE, NESLIB_SPRITE_GONE, scratch3+0x11, scratch4, (FIRST_ENEMY_SPRITE_ID + 12) + (i << 4));    
+        } else {
+            oam_spr(scratch, scratch2, scratch3, scratch4, FIRST_ENEMY_SPRITE_ID + (i << 4));
+            oam_spr(scratch+8, scratch2, scratch3+1, scratch4, (FIRST_ENEMY_SPRITE_ID + 4) + (i << 4));
+            oam_spr(scratch, scratch2+8, scratch3+0x10, scratch4, (FIRST_ENEMY_SPRITE_ID + 8) + (i << 4));
+            currentSpriteId = oam_spr(scratch+8, scratch2+8, scratch3+0x11, scratch4, (FIRST_ENEMY_SPRITE_ID + 12) + (i << 4));    
+        }
+
+	}
+	oam_hide_rest(currentSpriteId);
+
+}
+
+void banked_update_sprites() {
+
+}
---
+++ src/sprite.h
@@ -0,0 +1,35 @@
+#define SPRITE_BANK 0
+
+#define SPRITE_PALETTE_0 0
+#define SPRITE_PALETTE_1 1
+#define SPRITE_PALETTE_2 2
+#define SPRITE_PALETTE_3 3
+
+#define SPRITE_DIRECTION_LEFT 0x24
+#define SPRITE_DIRECTION_RIGHT 0x20
+#define SPRITE_DIRECTION_UP 4
+#define SPRITE_DIRECTION_DOWN 0
+#define SPRITE_DIRECTION_UNDEF 0xff
+
+#define SPRITE_SIZE_NORMAL 0
+
+#define SPRITE_TYPE_IGNORED 0x11
+#define SPRITE_TYPE_ENEMY 0x12
+#define SPRITE_TYPE_GEM 0x13
+
+#define SPRITE_PALETTE_MASK 0x03
+#define SPRITE_ANIM_MASK 0x30
+
+#define SPRITE_ANIM_NONE 0
+#define SPRITE_ANIM_DEFAULT 0x10
+#define SPRITE_ANIM_FULL 0x20
+
+#define NESLIB_SPRITE_GONE 0xf0
+
+// TODO: Do we wanna be smarter about sprite size?
+#define SPRITE_WIDTH 16
+#define SPRITE_HEIGHT 16
+
+
+void banked_draw_sprites();
+void banked_update_sprites();
